caa7d639e74242c7085dcbaae1bf274823d8cc2b
赵鹏飞 <vim.zpf@gmail.com>
2025-04-15 09:24:53 +0800

    适配器模式代码实现

---
+++ src/test/java/com/example/design/Adapter/AdvancedMediaPlayer.java
@@ -0,0 +1,6 @@
+package com.example.design.Adapter;
+
+public interface AdvancedMediaPlayer {
+   public void playVlc(String fileName);
+   public void playMp4(String fileName);
+}
---
+++ src/test/java/com/example/design/Adapter/MediaPlayer.java
@@ -0,0 +1,5 @@
+package com.example.design.Adapter;
+
+public interface MediaPlayer {
+   public void play(String audioType, String fileName);
+}
