bdf365f28e78fd0b8a82ebbc0f20715c06f26daa
赵鹏飞 <vim.zpf@gmail.com>
2025-05-29 17:20:17 +0800

    命令模式

---
+++ src/main/java/com/example/design/Command/Order.java
@@ -0,0 +1,5 @@
+package com.example.design.Command;
+
+public interface Order {
+   void execute();
+}
---
+++ src/main/java/com/example/design/Command/SellStock.java
@@ -0,0 +1,13 @@
+package com.example.design.Command;
+
+public class SellStock implements Order {
+   private Stock abcStock;
+ 
+   public SellStock(Stock abcStock){
+      this.abcStock = abcStock;
+   }
+ 
+   public void execute() {
+      abcStock.sell();
+   }
+}
