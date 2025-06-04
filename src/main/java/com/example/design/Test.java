2829c64e388499476c9b95756df1db33ff3e015a
赵鹏飞 <vim.zpf@gmail.com>
2025-06-04 13:36:27 +0800

    命令模式

---
+++ src/main/java/com/example/design/Test.java
@@ -0,0 +1,16 @@
+package com.example.design;
+
+/**
+ * description：TODO
+ * time：2025/6/4 08:51
+ * auther：zhaopengfei
+ */
+public class Test {
+
+    public static final String TAG = "";
+
+    public static void main(String[] args) {
+
+//        TAG = "Tests";
+    }
+}
